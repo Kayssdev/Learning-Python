c588627f9bb16edc324be4cc01d3b387e18f1620
Kayky <kaykysilva1303@gmail.com>
2026-05-27 21:24:55 -0300

    Create Ex.002.py

---
+++ Exercícios/Ex.002.py
@@ -0,0 +1,26 @@
+# today was just a simple coding practical exercise
+# trying to improve on while, break and continue.
+while True:
+    count = int(input("Enter a number: "))
+    if count == 0:
+        print("Exiting")
+        break
+
+    if count < 0:
+        print("Negative number entered, try again.")
+        continue
+
+# My first code 
+
+count = 5
+while count > 0:
+    user_input = input("Enter a number: ")
+    if user_input == "exit":
+        print("Exiting the program.")
+        break
+    if int(user_input) % 2 == 0:
+        print(f"{user_input} is even.")
+    else:        print(f"{user_input} is odd.")
+    count -= 1
+    if user_input == "0":
+        break
